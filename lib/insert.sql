4fa74140f7850bc8cd4eeb74d9cad7108d378449
Bianca Aspin <bianca@mj94.net>
2023-02-25 23:50:51 -0800

    Complete lab.

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Mr. Chocolate", 4, "M", "Dark Brown", "Smooth", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Rowdy", 7, "M", "Light Brown", "Aggressive", 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Tabitha", 9, "F", "White", "Nurturing", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Sergeant Brown", 11, "M", "Orange", "Alert", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Melissa", 3, "F", "Pink", "Cuddly", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Grinch", 20, "M", "Green", "Hermit", 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Wendy", 1, "F", "Blue", "Childish", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (NULL, 4, "M", "Black", "Mysterious", 1);
